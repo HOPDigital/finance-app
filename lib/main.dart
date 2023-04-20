4cbb416e5aedaab5d1a53076940ac97c9abd9400
andrelara2002 <andrelara2002@gmail.com>
2023-04-19 23:18:16 -0300

    Revert "Feat: Adding new screens and widgets"
    This reverts commit f528f331a4ef11305cd0d59691c400efd81803cc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/pages/boarding/first_boarding.dart';
-import 'package:flutter_application/source/model/CreditCard.dart';
-import 'package:flutter_application/utils/styles/colors.dart';
-import 'package:flutter_application/widgets/CreditCard.dart';
 
 void main() => runApp(const MainApp());
 
@@ -12,6 +8,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: FirstBoardingScreen());
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
   }
 }
