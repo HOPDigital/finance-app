795d88488a952eda97d018ff3758c1d209e16adf
Matt <matheusc039@hotmail.com>
2023-04-20 15:03:23 -0300

    refactor: using text style from typography class

--- lib/widgets/app_large_text.dart
+++ lib/widgets/app_large_text.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/utils/styles/typography.dart';
 
 class AppLargeText extends StatelessWidget {
   final String text;
-  final Color color;
-  final double size;
 
   const AppLargeText({
-    super.key,
+    Key? key,
     required this.text,
-    this.color = Colors.white,
-    this.size = 48,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(
-        fontSize: size,
-        color: color,
-        fontWeight: FontWeight.bold,
-      ),
+      style: AppTypography.heroText
     );
   }
 }
