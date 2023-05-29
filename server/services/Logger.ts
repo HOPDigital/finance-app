649ccb53e27c573d8cc2e90b40eb7dc297b59a79
André Lara <andrelara2002@gmail.com>
2023-05-29 20:28:08 +0000

    Fix: Fixing logs breach

--- server/services/Logger.ts
+++ server/services/Logger.ts
@@ -11,7 +11,7 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: '../logs/logs.log' })
+        new winston.transports.File({ filename: './logs/logs.log' })
     ]
 })
 
