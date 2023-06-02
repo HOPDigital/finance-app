e4d2f784f9093c099641ddeb8b963f1dd1218aeb
Matt <matheusc039@hotmail.com>
2023-06-01 22:29:36 -0300

    feat: added layout to dashboard cards

--- client/lib/pages/dashboard/dashboard_card.dart
+++ client/lib/pages/dashboard/dashboard_card.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 // Utils
 import 'package:flutter_application/utils/helpers/app_layout.dart';
 import 'package:flutter_application/utils/styles/colors.dart';
+import 'package:flutter_application/utils/styles/typography.dart';
 
 class DashboardCard extends StatelessWidget {
-  const DashboardCard({super.key});
+  const DashboardCard({super.key, required this.accountType, this.currentBalance = "--.---,--"});
+
+  final String accountType;
+  final String currentBalance;
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +23,41 @@ class DashboardCard extends StatelessWidget {
       height: AppLayout.getHeight(540),
       width: size.width * 0.85,
       margin: EdgeInsets.only(right: AppLayout.getHeight(15)),
+      padding: EdgeInsets.symmetric(
+        horizontal: AppLayout.getWidth(35),
+        vertical: AppLayout.getHeight(25),
+      ),
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Text(
+            accountType,
+            style: AppTypography.titleOne.copyWith(color: Colors.black),
+            textAlign: TextAlign.center,
+          ),
+          Positioned(
+            bottom: 10,
+            left: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currentBalance,
+                  style: AppTypography.titleOne.copyWith(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                Text(
+                  "Current balance",
+                  style: AppTypography.footnote.copyWith(color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
