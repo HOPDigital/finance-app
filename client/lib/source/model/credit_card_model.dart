df45b3174cb949179a4c2c302c5b0ff583ebf7cb
Matt <matheusc039@hotmail.com>
2023-06-08 18:22:59 -0300

    fixed design of credit card

--- client/lib/source/model/credit_card_model.dart
+++ client/lib/source/model/credit_card_model.dart
@@ -1,12 +1,11 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_application/utils/styles/colors.dart';
 
 class CreditCard {
   final String name;
   final String currency;
-  final Color color = AppColors.blueColor;
+  final Color color;
+  final Color textColor;
 
   final int number;
   final String date;
@@ -15,6 +14,8 @@ class CreditCard {
   CreditCard({
     required this.number,
     required this.balance,
+    required this.color,
+    required this.textColor,
     this.currency = 'USD',
     this.name = 'My Personal Card',
     this.date = '01/23',
--- client/lib/widgets/credit_card/credit_card.dart
+++ client/lib/widgets/credit_card/credit_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/source/model/credit_card_model.dart';
-import 'package:flutter_application/utils/styles/colors.dart';
 import 'package:flutter_application/utils/styles/typography.dart';
 
 class CreditCardWidget extends StatelessWidget {
@@ -15,51 +14,75 @@ class CreditCardWidget extends StatelessWidget {
       height: 200,
       width: 350,
       padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.only(right: 10.0),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: AppColors.blueColor,
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey.withOpacity(.4),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: const Offset(0, 3))
-          ]),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        borderRadius: BorderRadius.circular(20),
+        color: card.color,
+        boxShadow: [
+          BoxShadow(
+              color: card.color.withOpacity(.2),
+              spreadRadius: 3,
+              blurRadius: 3,
+              offset: const Offset(0, 3))
+        ],
+      ),
+      child: Stack(
         children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              card.name,
-              style: AppTypography.titleOne,
-            ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      card.name,
+                      style: AppTypography.titleOne.copyWith(
+                        color: card.textColor,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '**** **** **** ${card.number.toString()}',
+                      style: AppTypography.subhead.copyWith(
+                        color: card.textColor,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      card.date,
+                      style: AppTypography.subhead.copyWith(
+                        color: card.textColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  '${card.currency} ${card.balance.toStringAsFixed(2)}',
+                  style: AppTypography.largeTitle.copyWith(
+                    color: card.textColor,
+                  ),
+                ),
+              ),
+            ],
           ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              '**** **** **** ${card.number.toString()}',
-              style: AppTypography.subhead,
+          Positioned(
+            bottom: 10.0,
+            right: 0,
+            child: Image.asset(
+              'assets/images/visa-logo.png',
+              semanticLabel: "Black Visa Logo",
+              filterQuality: FilterQuality.medium,
             ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              card.date,
-              style: AppTypography.subhead,
-            ),
-          ),
-          const SizedBox(height: 10),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              '${card.currency} ${card.balance.toStringAsFixed(2)}',
-              style: AppTypography.heroText,
-            ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text('Current balance', style: AppTypography.subhead),
           ),
         ],
       ),
