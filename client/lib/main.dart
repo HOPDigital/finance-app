416ac046b7795cf74a7d53de72edb1cc29c72f77
Matt <matheusc039@hotmail.com>
2023-06-22 16:27:14 -0300

    feat: removed both status bar and navigation bar (Android) with system overlay

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application/pages/boarding/boarding_screen.dart';
 import 'package:flutter_application/pages/check_user_account.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_application/pages/splash_intro_screen.dart';
 
-Future main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final prefs = await SharedPreferences.getInstance();
   final showCheckUserAccount = prefs.getBool('showCheckUserAccount') ?? false;
 
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky, overlays: [ SystemUiOverlay.bottom ]);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   runApp(MainApp(showCheckUserAccount: showCheckUserAccount));
 }
 
